052f760a55a3cd17099a6d3710432fb6eeaf04ee
srush2004 <psrushti807@gmail.com>
2024-12-02 18:02:16 +0000

    variable

---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,14 @@
+const account_Id = 85415
+let accountEmail = "srush4533@sc.com"
+var accountPassword = "23e32"
+accountcity = "dwarika"
+
+//account_Id = 3 // not allowed
+
+accountEmail = "happy@sc.com"
+accountPassword = "54255"
+accountcity = "vrundavan"
+
+console.log(account_Id);
+
+console.table([account_Id,accountEmail,accountPassword,accountcity])
