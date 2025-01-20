8e0b7bfaa3c7bfd723a24c2f4d73da3899f2194e
srush2004 <psrushti807@gmail.com>
2025-01-20 12:29:37 +0000

    datatype

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)// we sre using node js not browser
+
+console.log(3+3)
+console.log("sru")
+
+let name = "evu"
+let age = "2"
+let isloggedIn = false
+
+//number = 2 to power 53
+//bigint 
+//string = ""
+//boolean = true/false
+//null = standalone value
+// undefined means we don't defined any value
+// symbol = unique 
+
+//object 
+
+console.log(typeof "sru");
+
+//null type  is object and undefined type is undefined itself
