5cef655addf9fa3dfab0c23fbf9b3bec9d03e53f
srush2004 <psrushti807@gmail.com>
2025-01-20 13:05:40 +0000

    conversionOperation.js

---
+++ .devcontainer/basics/conversionOperation.js
@@ -0,0 +1,27 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); 
+
+// "5" = 5
+// "5abc" = NaN 
+// true = 1; false = 0;
+
+let isloggedIn = 1
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(booleanIsloggedIn);
+
+// 1= true ;  0 = false
+//"" = false
+// "sru" = true
+
+let someNumber = 5
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
